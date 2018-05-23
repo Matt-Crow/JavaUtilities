ed37ba9d6dc52c9b015233da0ec48f4474e09621
IronHeart7334 <greengrappler12@gmail.com>
2018-05-23 11:07:36 -0700

    i

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,10 @@
 package main;
 import deckBuilder.DeckBuilderFrame;
+import graphics.ColorTesterFrame;
 
 public class Main {
 	public static void main(String[] args) {
-		new DeckBuilderFrame();
+		//new DeckBuilderFrame();
+		new ColorTesterFrame();
 	}
 }
