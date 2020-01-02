ca69520093d2f5f854799680cd6dc5b73210dbd8
IronHeart7334 <greengrappler12@gmail.com>
2020-01-02 14:42:15 -0800

    Only seems to work with headphones

---
+++ src/main/java/start/Main.java
@@ -0,0 +1,51 @@
+package start;
+
+import java.util.Arrays;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.TargetDataLine;
+
+/**
+ *
+ * @author Matt
+ */
+public class Main {
+    public static void main(String[] args){
+        AudioFormat format = new AudioFormat(8000.0f, 16, 1, true, true);
+        
+        try {
+            TargetDataLine microphone = AudioSystem.getTargetDataLine(format);
+            SourceDataLine speakers = AudioSystem.getSourceDataLine(format);
+            
+            DataLine.Info microphoneInfo = new DataLine.Info(TargetDataLine.class, format);
+            microphone = (TargetDataLine)AudioSystem.getLine(microphoneInfo);
+            
+            DataLine.Info speakerInfo = new DataLine.Info(SourceDataLine.class, format);
+            speakers = (SourceDataLine)AudioSystem.getLine(speakerInfo);
+            
+            microphone.open(format);
+            speakers.open(format);
+            microphone.start();
+            speakers.start();
+            
+            int i = 0;
+            int numBytesRead = 0;
+            byte[] buff = new byte[microphone.getBufferSize()];
+            while(i < 99999){
+                numBytesRead = microphone.read(buff, 0, microphone.getBufferSize());
+                speakers.write(buff, 0, numBytesRead);
+                i++;
+                System.out.println(Arrays.toString(buff));
+            }
+            speakers.drain();
+            microphone.close();
+            speakers.close();
+        } catch (LineUnavailableException ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+}
