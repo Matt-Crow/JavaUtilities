42c0cdae0da78080903792837a33d1dbf2099091
IronHeart7334 <greengrappler12@gmail.com>
2020-06-14 16:49:57 -0700

    yay it logs what I type. I is such a good programmer

--- src/main/java/gui/TextChatComponent.java
+++ src/main/java/gui/TextChatComponent.java
@@ -1,17 +1,21 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JComponent;
+import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingUtilities;
 
 /**
  * split this into multiple classes?
  * @author Matt
  */
-public class TextChatComponent extends JComponent{
+public class TextChatComponent extends JComponent implements ActionListener{
     private final JTextArea msgs;
     private final JScrollPane scroll;
     private final JTextField input;
@@ -32,6 +36,37 @@ public class TextChatComponent extends JComponent{
         add(scroll, BorderLayout.CENTER);
          
         input = new JTextField();
+        input.addActionListener(this);
         add(input, BorderLayout.PAGE_END);
+    }
+    
+    /**
+     * Fired whenever the user enters something into the input field
+     * @param e 
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String msg = input.getText();
+        if(msg.startsWith("/")){
+            //is a command
+        } else {
+            send(msg);
+        }
+        
+        input.setText("");
+    }
+    private void logLocal(String message){
+        msgs.append(message);
+        msgs.append("\n");
+        SwingUtilities.invokeLater(()->{
+            JScrollBar bar = scroll.getVerticalScrollBar();
+            bar.setValue(bar.getMaximum());
+            scroll.repaint();
+        });
+    }
+    
+    private void send(String message){
+        // do server stuff in the future
+        logLocal(String.format("You: %s", message));
     }
 }
