ef45f2f725a8493246e8e0c1cdbaafc6e1ca3a90
IronHeart7334 <greengrappler12@gmail.com>
2020-06-14 17:06:52 -0700

    Yay! Commands!

--- src/main/java/gui/TextChatComponent.java
+++ src/main/java/gui/TextChatComponent.java
@@ -3,6 +3,8 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.function.Consumer;
 import javax.swing.JComponent;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
@@ -19,7 +21,9 @@ public class TextChatComponent extends JComponent implements ActionListener{
     private final JTextArea msgs;
     private final JScrollPane scroll;
     private final JTextField input;
-    // TODO: add commands
+    private final HashMap<String, Consumer<String>> commands;
+    
+    private static final String CMD_START = "/";
     
     public TextChatComponent(){
         super();
@@ -38,6 +42,10 @@ public class TextChatComponent extends JComponent implements ActionListener{
         input = new JTextField();
         input.addActionListener(this);
         add(input, BorderLayout.PAGE_END);
+        
+        commands = new HashMap<>();
+        addCommand("?", (str)->listCommands());
+        addCommand("echo", (str)->logLocal(String.format("Echo: %s", str)));
     }
     
     /**
@@ -47,14 +55,27 @@ public class TextChatComponent extends JComponent implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         String msg = input.getText();
-        if(msg.startsWith("/")){
+        if(msg.startsWith(CMD_START)){
             //is a command
+            int firstSpace = msg.indexOf(" ");
+            String inputtedCmd = (firstSpace == -1) ? msg.substring(1) : msg.substring(1, firstSpace).toUpperCase();
+            String args = (firstSpace == -1) ? "" : msg.substring(firstSpace + 1);
+            if(commands.containsKey(inputtedCmd)){
+                commands.get(inputtedCmd).accept(args);
+            } else {
+                logLocal(String.format("Invalid command: %s", inputtedCmd));
+            }
         } else {
             send(msg);
         }
         
         input.setText("");
     }
+    
+    public void addCommand(String name, Consumer<String> f){
+        commands.put(name.toUpperCase(), f);
+    }
+    
     private void logLocal(String message){
         msgs.append(message);
         msgs.append("\n");
@@ -68,5 +89,12 @@ public class TextChatComponent extends JComponent implements ActionListener{
     private void send(String message){
         // do server stuff in the future
         logLocal(String.format("You: %s", message));
+    }
+    
+    private void listCommands(){
+        logLocal("Available commands:");
+        commands.keySet().forEach((cmdName)->{
+            logLocal(String.format("* %s%s", CMD_START, cmdName));
+        });
     }
 }
