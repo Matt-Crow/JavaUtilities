6441d7c33f4ce52c241f5bce0624c7d54bbf4c33
IronHeart7334 <greengrappler12@gmail.com>
2020-07-29 15:46:47 -0700

    Not happy with Orpheus right now

---
+++ src/main/java/net/ChatServer.java
@@ -0,0 +1,121 @@
+package net;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
+import java.util.HashSet;
+
+/**
+ * https://stackoverflow.com/questions/2983835/how-can-i-interrupt-a-serversocket-accept-method
+ * @author Matt
+ */
+public class ChatServer {
+    private final ServerSocket server;
+    private final HashSet<InetAddress> validIpAddrs;
+    private Thread connectionListenerThread;
+    
+    private static final int PORT = 4999;
+    private static ChatServer instance;
+    
+    private ChatServer() throws IOException {
+        if(instance != null){
+            throw new ExceptionInInitializerError("ChatServer is a singleton: Use getInstance instead of invoking the constructor");
+        }
+        server = new ServerSocket(PORT);
+        validIpAddrs = findValidIps();
+        startConnectionListenerThread();
+    }
+    
+    public static void validateServer() throws IOException{
+        if(instance == null){
+            instance = new ChatServer();
+        }
+    }
+    
+    public static ChatServer getInstance() {
+        if(instance == null){
+            throw new NullPointerException("Make sure you call ChatServer.validateServer() before ChatServer.getInstance()!");
+        }
+        return instance;
+    }
+    
+    private HashSet<InetAddress> findValidIps() throws SocketException{
+        HashSet<InetAddress> ips = new HashSet<>();
+        
+        // get network interfaces
+        Enumeration<NetworkInterface> eni = NetworkInterface.getNetworkInterfaces();
+        NetworkInterface ni = null;
+        Enumeration<InetAddress> addrs = null;
+        InetAddress ia = null;
+        
+        while(eni.hasMoreElements()){
+            ni = eni.nextElement();
+            if(!ni.isLoopback() && ni.isUp()){
+                //System.out.println(ni);
+                addrs = ni.getInetAddresses();
+                while(addrs.hasMoreElements()){
+                    ia = addrs.nextElement();
+                    //System.out.println(ia.getHostAddress());
+                    ips.add(ia);
+                }
+            }
+        }
+        return ips;
+    }
+    
+    private void startConnectionListenerThread(){
+        if(connectionListenerThread == null){
+            connectionListenerThread = new Thread(){
+                @Override
+                public void run(){
+                    Socket remoteComputer = null;
+                    while(true){
+                        try {
+                            remoteComputer = server.accept();
+                            System.out.println("Accept " + remoteComputer.getInetAddress().getHostAddress());
+                        } catch (SocketException ex){
+                            System.out.println("Server is closed. Breaking.");
+                            break;
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                    connectionListenerThread = null;
+                }
+            };
+            connectionListenerThread.start();
+            
+        }
+    }
+    
+    public void shutDown() throws IOException{
+        // Javadoc: "Any thread currently blocked in ServerSocket.accept() will throw a SocketException."
+        server.close();
+    }
+    
+    public static void main(String[] args){
+        try {
+            ChatServer.validateServer();
+        } catch (IOException ex) {
+            System.err.println("Server won't start :(");
+            ex.printStackTrace();
+            System.exit(1);
+        }
+        
+        ChatServer serv = ChatServer.getInstance();
+        try {
+            new Socket("0.0.0.0", PORT);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        try {
+            serv.shutDown();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
