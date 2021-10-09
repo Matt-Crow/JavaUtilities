66730ef905381e9f33ad14fadaece8aabe88e6de
Matt Crow <greengrappler12@gmail.com>
2021-10-09 16:55:58 -0700

    it works! Just need to add a GUI

--- PDFUtil/src/main/java/pdf/PageRotator.java
+++ PDFUtil/src/main/java/pdf/PageRotator.java
@@ -1,5 +1,6 @@
 package pdf;
 
+import java.awt.Rectangle;
 import java.io.IOException;
 import java.util.Iterator;
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -10,35 +11,37 @@ import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.util.Matrix;
 
 /**
- *
+ * A huge thank you to https://stackoverflow.com/questions/40611736/rotate-pdf-around-its-center-using-pdfbox-in-java!
  * @author Matt Crow
  */
 public class PageRotator {
-    // https://stackoverflow.com/questions/40611736/rotate-pdf-around-its-center-using-pdfbox-in-java
     public PDDocument rotateBy(PDDocument doc, int degreesClockwise){
         PDDocument newDoc = new PDDocument();
         Iterator<PDPage> iter = doc.getPages().iterator();
         PDPage page;
-        PDRectangle box;
-        float[] t;
+        Matrix rotation = Matrix.getRotateInstance(Math.toRadians(-degreesClockwise), 0, 0);
+        Rectangle bounds;
+        PDRectangle newBox;
         while(iter.hasNext()){
             page = iter.next();
-            box = page.getCropBox();
-            t = new float[]{
-                (box.getLowerLeftX() + box.getUpperRightX()) / 2,
-                (box.getLowerLeftY() + box.getUpperRightY()) / 2
-            };
             try(
                 PDPageContentStream s = new PDPageContentStream(newDoc, page, PREPEND, false);
             ){
-                s.transform(Matrix.getTranslateInstance(t[0], t[1]));
-                s.transform(Matrix.getRotateInstance(Math.toRadians(-degreesClockwise), 0, 0));
-                s.transform(Matrix.getTranslateInstance(-t[0], -t[1]));
+                s.transform(rotation);                
             } catch (IOException ex) {
                 throw new PDFException(ex);
             }
+            
+            bounds = page.getCropBox().transform(rotation).getBounds();
+            newBox = new PDRectangle(
+                (float)bounds.getX(),
+                (float)bounds.getY(),
+                (float)bounds.getWidth(),
+                (float)bounds.getHeight()
+            );
+            page.setCropBox(newBox);
+            page.setMediaBox(newBox);
             newDoc.addPage(page);
-            System.out.println("rotate");
         }
         return newDoc;
     }
--- PDFUtil/src/main/java/start/main.java
+++ PDFUtil/src/main/java/start/main.java
@@ -12,7 +12,7 @@ import pdf.PageRotator;
 public class main {
     public static void main(String... args){
         System.out.println("Works!");
-        if(args.length >= 1){
+        if(true || args.length >= 1){
             PDFReader reader = new PDFReader(new File(args[0]));
             PDDocument doc = reader.read();
             PDDocument rotated = new PageRotator().rotateBy(doc, 90);
