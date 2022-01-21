acd3b9c02cec5ab1350f0dbf509b30844b1361f5
Matt Crow <greengrappler12@gmail.com>
2022-01-21 15:21:28 -0800

    migrated all of these over to the application plugin

--- BoardGameSimulator/src/main/java/boardgameSimulator/deckBuilder/DeckBuilderFrame.java
+++ BoardGameSimulator/src/main/java/boardgameSimulator/deckBuilder/DeckBuilderFrame.java
@@ -1,19 +1,22 @@
 package boardgameSimulator.deckBuilder;
 
 import java.awt.Toolkit;
-
 import javax.swing.JFrame;
 
-@SuppressWarnings("serial")
-public class DeckBuilderFrame extends JFrame{
-	public DeckBuilderFrame(){
-		super();
-		setTitle("Deckbuilder");
-		setSize((int) Toolkit.getDefaultToolkit().getScreenSize().getWidth(), 
-				(int) Toolkit.getDefaultToolkit().getScreenSize().getHeight() 
-				- Toolkit.getDefaultToolkit().getScreenInsets(this.getGraphicsConfiguration()).bottom);
-		setContentPane(new DeckBuilderPanel());
-		setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+public class DeckBuilderFrame extends JFrame {
+
+    public DeckBuilderFrame() {
+        super();
+        setTitle("Deckbuilder");
+        setSize((int) Toolkit.getDefaultToolkit().getScreenSize().getWidth(),
+                (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight()
+                - Toolkit.getDefaultToolkit().getScreenInsets(this.getGraphicsConfiguration()).bottom);
+        setContentPane(new DeckBuilderPanel());
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args){
+        new DeckBuilderFrame();
+    }
 }
