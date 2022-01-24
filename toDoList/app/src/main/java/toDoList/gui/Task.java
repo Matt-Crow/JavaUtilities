e047a3248f958bbb8b4f0e9adfc688a83a5c340f
Matt Crow <greengrappler12@gmail.com>
2022-01-24 12:54:47 -0800

    since I have about 3 hours until my next class, I'm starting work on this tool. Netbeans is really buggy with Java indentation...

---
+++ toDoList/app/src/main/java/toDoList/gui/Task.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package toDoList.gui;
+
+import java.util.Date;
+
+/**
+ * @author Matt
+ */
+public class Task {
+    
+    private final String subject;
+    private final String description;
+    private final Date dueDate;
+
+    public Task(String subject, String description, Date dueDate){
+        this.subject = subject;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+
+    public final String getSubject(){
+        return subject;
+    }
+
+    public final String getDescription(){
+        return description;
+    }
+
+    public final Date getDueDate(){
+        return dueDate;
+    }
+}
