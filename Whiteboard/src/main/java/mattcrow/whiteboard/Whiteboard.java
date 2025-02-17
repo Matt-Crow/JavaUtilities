562974495300a131b8d0b20ea350a0691ab2fe96
Matt Crow <mattcrow19@gmail.com>
2025-02-17 11:29:55 -0800

    Draw on buffer instead of point class for #2

--- Whiteboard/src/main/java/mattcrow/whiteboard/Whiteboard.java
+++ Whiteboard/src/main/java/mattcrow/whiteboard/Whiteboard.java
@@ -6,79 +6,63 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.*;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.awt.image.BufferedImage;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
-import javax.swing.SwingUtilities;
 
 /**
  *
  * @author Matt
  */
-public class Whiteboard extends JPanel{
-    private final LinkedList<Point> points = new LinkedList<>();
+public class Whiteboard extends JPanel {
+    // TODO allow resizing
+    private final BufferedImage buffer = new BufferedImage(500, 500, BufferedImage.TYPE_INT_ARGB);
+
     private final int MARKER_SIZE = 10;
     private final JPanel body;
-    
-    public Whiteboard(){
-        super();
 
+    public Whiteboard() {
+        super();
         setLayout(new BorderLayout());
         body = new JPanel() {
             @Override
-            public void paintComponent(Graphics g){
+            public void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                g.setColor(Color.BLACK);
-                Iterator<Point> i = points.iterator();
-                Point p;
-                while(i.hasNext()){
-                    p = i.next();
-                    g.fillRect(p.x, p.y, MARKER_SIZE, MARKER_SIZE);
-                }
+                g.drawImage(buffer, 0, 0, this);
             }
         };
         add(body, BorderLayout.CENTER);
         body.setBackground(Color.WHITE);
-        initMouseAdapter();
+        MouseAdapter a1 = new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                for (var dx = 0; dx < MARKER_SIZE; dx++) {
+                    for (var dy = 0; dy < MARKER_SIZE; dy++) {
+                        buffer.setRGB(e.getX() + dx, e.getY() + dy, Color.BLACK.getRGB()); // TODO allow choosing color
+                    }
+                }
+                repaint();
+            }
+        };
+        body.addMouseMotionListener(a1);
 
         var toolsSection = new JPanel();
         add(toolsSection, BorderLayout.PAGE_START);
 
         SaveButton s = new SaveButton(new SaveAction(body));
         toolsSection.add(s);
-        
-        //temp
+
+        // temp
         JButton load = new JButton("Load");
-        load.addActionListener((a)->{
+        load.addActionListener((a) -> {
             JFileChooser choose = new JFileChooser();
             choose.setFileSelectionMode(JFileChooser.FILES_ONLY);
-            if(choose.showOpenDialog(this) == JFileChooser.APPROVE_OPTION){
+            if (choose.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
                 throw new UnsupportedOperationException();
             }
         });
         toolsSection.add(load);
-        
+
         repaint();
-    }
-    
-    private void initMouseAdapter(){
-        MouseAdapter a = new MouseAdapter(){
-            @Override
-            public void mouseDragged(MouseEvent e){
-                /*
-                The points are rounded to a rectangular grid
-                so that it is easier to erase them
-                */
-                //if left mouse button, draw, if right, erase
-                if(SwingUtilities.isLeftMouseButton(e)){
-                    points.add(new Point(e.getX() - e.getX() % MARKER_SIZE, e.getY() - e.getY() % MARKER_SIZE));
-                } else if(SwingUtilities.isRightMouseButton(e)){
-                    points.remove(new Point(e.getX() - e.getX() % MARKER_SIZE, e.getY() - e.getY() % MARKER_SIZE));
-                }
-                repaint();
-            }
-        };
-        body.addMouseMotionListener(a);
     }
 }
